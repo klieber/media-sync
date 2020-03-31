1fa0ff9690a25c7586ced0e9df6057abbb669a04
Kyle Lieber <klieber@users.noreply.github.com>
2020-03-31 17:45:53 -0500

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,39 @@
+module.exports = {
+  'env': {
+    'commonjs': true,
+    'es6': true,
+    'jest': true,
+    'node': true
+  },
+  'extends': 'eslint:recommended',
+  'globals': {
+    'Atomics': 'readonly',
+    'SharedArrayBuffer': 'readonly'
+  },
+  'parser': 'babel-eslint',
+  'parserOptions': {
+    'ecmaVersion': 2019
+  },
+  'rules': {
+    'indent': [
+      'error',
+      2
+    ],
+    'linebreak-style': [
+      'error',
+      'unix'
+    ],
+    'quotes': [
+      'error',
+      'single'
+    ],
+    'semi': [
+      'error',
+      'always'
+    ],
+    'comma-dangle': [
+      'error',
+      'never'
+    ]
+  }
+};
