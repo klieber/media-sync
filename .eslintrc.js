b5b48eecec65a4a30c6f665215e624b29c80c748
Kyle Lieber <klieber@users.noreply.github.com>
2020-04-02 21:06:50 -0500

    chore: integrate prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,39 +1,24 @@
 module.exports = {
-  'env': {
-    'commonjs': true,
-    'es6': true,
-    'jest': true,
-    'node': true
+  env: {
+    es6: true,
+    node: true,
+    jest: true
   },
-  'extends': 'eslint:recommended',
-  'globals': {
-    'Atomics': 'readonly',
-    'SharedArrayBuffer': 'readonly'
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended'
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly'
   },
-  'parser': 'babel-eslint',
-  'parserOptions': {
-    'ecmaVersion': 2019
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module'
   },
-  'rules': {
-    'indent': [
-      'error',
-      2
-    ],
-    'linebreak-style': [
-      'error',
-      'unix'
-    ],
-    'quotes': [
-      'error',
-      'single'
-    ],
-    'semi': [
-      'error',
-      'always'
-    ],
-    'comma-dangle': [
-      'error',
-      'never'
-    ]
-  }
+  plugins: ['@typescript-eslint'],
+  rules: {}
 };
