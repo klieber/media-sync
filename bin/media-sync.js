19f43c4fc37ff48673c8fc4332426c6674168158
Kyle Lieber <klieber@users.noreply.github.com>
2020-04-05 01:16:19 -0500

    refactor: create dropbox provider through factory

--- bin/media-sync.js
+++ bin/media-sync.js
@@ -2,38 +2,22 @@
 
 require('dotenv').config();
 
-const DropboxService = require('../lib/provider/dropbox-service');
-const DropboxProvider = require('../lib/provider/dropbox-provider');
+const provider = require('../lib/provider').create();
 const ImageHandler = require('../lib/handler/image-handler');
 const { asyncForEach } = require('../lib/support/async-utils');
-
-const { Dropbox } = require('dropbox');
-
-const dropboxClient = new Dropbox({
-  fetch: require('node-fetch'),
-  accessToken: process.env.DROPBOX_ACCESS_TOKEN
-});
-
-const config = {
-  source: process.env.SOURCE_PATH,
-  target: process.env.TARGET_PATH
-};
-
-const dropboxService = new DropboxService(dropboxClient);
-const dropboxProvider = new DropboxProvider(dropboxService, config.source, config.target);
 
 const handlers = [new ImageHandler()];
 
 (async () => {
   try {
-    const files = await dropboxProvider.list();
+    const files = await provider.list();
 
     // TODO: remove the slice call
     await asyncForEach(files.slice(0, 5), async (file) => {
       const handler = handlers.find((handler) => handler.supports(file.path_lower));
       if (handler) {
         try {
-          const filename = await dropboxProvider.download(file);
+          const filename = await provider.download(file);
           handler.handle(filename);
         } catch (error) {
           console.log(`Unable to download file: ${file.path_lower}`, error);
