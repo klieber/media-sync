b1ee0ebdb3478b6f6eda93c216cb9e6e27ceefcf
Kyle Lieber <klieber@users.noreply.github.com>
2025-01-28 23:25:37 -0600

    build(deps): upgrade devDependencies

---
+++ eslint.config.mjs
@@ -0,0 +1,33 @@
+import globals from 'globals';
+import babelParser from '@babel/eslint-parser';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import js from '@eslint/js';
+import { FlatCompat } from '@eslint/eslintrc';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all
+});
+
+export default [
+  ...compat.extends('eslint:recommended', 'plugin:prettier/recommended'),
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.jest,
+        Atomics: 'readonly',
+        SharedArrayBuffer: 'readonly'
+      },
+      parser: babelParser,
+      ecmaVersion: 2019,
+      sourceType: 'module'
+    },
+
+    rules: {}
+  }
+];
